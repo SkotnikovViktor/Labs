f7038c5ac379857ff93648f5ced96f6c7e83ba8c
Viktor <skotnikovviktor82@gmail.com>
2025-10-22 21:50:52 +0300

    Добавлены некоторые задания из 8 лабы(НЕ ПРОВЕРЕНО)

---
+++ 8labs/1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+double fact(long long number){
+
+    if (number < 50){
+        long long result = 1.0;
+
+        for (int i = 1; i <= number; i++){
+            result *= i;
+        }
+
+
+        return result;
+    }
+
+    else{
+        return pow(number,number) / 1.0;
+    }
+}
+
+
+
+int main(void){
+    
+    double toch;
+    double x = 0.6;
+    double start_number = 1.0;
+    double cos_x = cos(x);
+    int m = 1;
+
+
+    scanf("%lf", &toch); 
+
+
+    for (int i = 2;; i+=2){
+        m = m * (-1);
+        start_number = start_number + (pow(x,i) / fact(i)) * m;
+
+        if (fabs(cos_x - start_number) < toch){
+
+            printf("Результат - %lf\nЗначение из math - %lf\n",start_number,cos_x);
+            break;
+
+        }
+        
+
+    }
+
+
+    
+
+
+    
+    
+
+
+
+}
